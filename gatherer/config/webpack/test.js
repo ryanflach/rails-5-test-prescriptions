4b11b01eb3e9fe8eb8ae6985ae3c53ca465b3fa6
Ryan Flach <ryanflach@users.noreply.github.com>
2018-07-16 16:22:06 -0600

    Add configurations for JavaScript unit testing and a sample spec
    - Complete through page 197

--- gatherer/config/webpack/test.js
+++ gatherer/config/webpack/test.js
@@ -1,5 +1,7 @@
-process.env.NODE_ENV = process.env.NODE_ENV || 'development'
-
 const environment = require('./environment')
 
-module.exports = environment.toWebpackConfig()
+environment.plugins.get('Manifest').opts.writeToFileEmit = process.env.NODE_ENV !== 'test'
+
+const config = environment.toWebpackConfig();
+config.devtool = 'inline-source-map'
+module.exports = config
